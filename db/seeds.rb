373d1dd8d3513bf4e439fa3b933d9bc10215db13
Yoshimine Sato <universato@gmail.com>
2020-08-14 14:56:42 +0900

    use page_entries_info(kaminari function)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
-5.upto(108) do |i|
+start_id = (Book.last&.id || 0) + 1
+end_id = start_id + 5
+start_id.upto(end_id) do |i|
   title = "title #{i.to_s.rjust(3)}"
   memo = "memo #{i.to_s.rjust(3)}"
   author = "author #{i.to_s.rjust(3)}"
   Book.create!(title: title, memo: memo, author: author)
+end
+
+start_id = (User.last&.id || 0) + 1
+end_id = start_id + 10
+start_id.upto(end_id) do |i|
+  email = "e#{i}@example.com"
+  password = "password"
+  password_confirmation = "password"
+  User.create!(email: email, password: password, password_confirmation: password_confirmation)
 end
